5aa981b4eb4277cc16bfb7ad16339c858d90485d
AlexFrancis95 <alexfrancis@email.com>
2018-10-27 11:51:23 +0100

    app.js file created and required needed ejs apps

---
+++ views/app.js
@@ -0,0 +1,22 @@
+const express = require('express'); // Access the express package
+const app = express();
+app.use(express.static('public'));
+
+const expressLayouts = require('express-ejs-layouts'); // Access EJS from Express
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+const session = require('express-session');
+app.use(session({ secret: 'shh...', resave: false, saveUninitialized: false }));
+
+const mongoose = require('mongoose');
+const env = require('./config/environment');
+mongoose.connect(env.dbUri);
+
+app.listen(env.port, () => console.log(`Express is listening on port ${env.port}`));

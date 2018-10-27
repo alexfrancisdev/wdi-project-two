81342953ebf96f89c2644cb7296b5259494878c3
AlexFrancis95 <alexfrancis@email.com>
2018-10-27 13:40:28 +0100

    added register/login/logout routes

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const authController = require('../controllers/authController');
+
+router.get('/register', authController.registerFormRoute);
+router.post('/register', authController.registerRoute);
+
+router.get('/login', authController.loginFormRoute);
+router.post('/login', authController.loginRoute);
+
+router.get('/logout', authController.logoutRoute);
 
 router.get('/', function(req, res) {
   res.render('home');

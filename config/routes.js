8ac695e849229c8adadd2c0fd18dafc2eeca5bcd
AlexFrancis95 <alexfrancis@email.com>
2018-10-29 10:59:32 +0000

    added update, edit and delete routes

--- config/routes.js
+++ config/routes.js
@@ -25,4 +25,13 @@ router.post('/posts', postController.createRoute);
 // SHOW Route
 router.get('/posts/:id', postController.showRoute);
 
+// UPDATE route
+router.put('/posts/:id', postController.updateRoute);
+
+// EDIT route
+router.get('/posts/:id/edit', postController.editRoute);
+
+// DELETE Route
+router.delete('/posts/:id', postController.deleteRoute);
+
 module.exports = router;
